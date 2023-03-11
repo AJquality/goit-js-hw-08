6de0e32357d616b3b92d8f4eb05f6a18d77966e4
Adrian Jurczuk <jurczuk.adrian@gmail.com>
2023-03-11 15:48:46 +0100

    Zadanie 1 modul 9 zrobione

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startButton = document.querySelector('[data-start]');
+startButton.disabled = false;
+const stopButton = document.querySelector('[data-stop]');
+stopButton.disabled = true;
+const body = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let functionInterval = null;
+
+function startRandomColor() {
+  functionInterval = setInterval(function () {
+    body.style.cssText = `background-color: ${getRandomHexColor()}`;
+  }, 1000);
+  startButton.disabled = true;
+  stopButton.disabled = false;
+}
+
+function stopRandomColor() {
+  functionInterval = clearInterval(functionInterval);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
+
+startButton.addEventListener('click', () => startRandomColor());
+
+stopButton.addEventListener('click', () => stopRandomColor());
